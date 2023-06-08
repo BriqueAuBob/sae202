9fcc20c51209c494ab64425d9e8d34d36924bb74
CassandreLAMATY <lamatycassandre@gmx.fr>
2023-06-08 13:35:15 +0200

    trajets affichage

--- assets/js/trip_parking.js
+++ assets/js/trip_parking.js
@@ -1,5 +1,38 @@
+const departure_city = document.querySelector('#departure_city');
+const departure_options = document.querySelector('#departure_options');
+const p1 = document.querySelector('#p1');
+const p2 = document.querySelector('#p2');
+
 const areas = document.querySelectorAll('rect');
 const departure_address = document.querySelector('#departure_address');
+
+departure_options.style.display = 'none';
+p1.style.display = 'none';
+p2.style.display = 'none';
+
+departure_city.addEventListener('input', () => {
+    let city = departure_city.value.toLowerCase();
+    if(city == 'troyes') {
+        departure_options.style.display = 'block';
+    } else {
+        departure_options.style.display = 'none';
+    }
+});
+
+departure_options.addEventListener('change', () => {
+    let option = departure_options.value;
+
+    if(option == 'p1') {
+        p1.style.display = 'block';
+        p2.style.display = 'none';
+    } else if(option == 'p2') {
+        p2.style.display = 'block';
+        p1.style.display = 'none';
+    } else {
+        p1.style.display = 'none';
+        p2.style.display = 'none';
+    }
+});
 
 areas.forEach(area => {
     area.addEventListener('click', () => {
