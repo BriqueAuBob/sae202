6a493ad48366ce68793afeafaeb4d62e93528af6
BriqueAuBob <briqueaubob@gmail.com>
2023-06-09 11:50:19 +0200

    page parkings

--- views/parkings.php
+++ views/parkings.php
@@ -2,5 +2,46 @@
 	<h1>Parkings</h1>
 </header>
 <section class="overlap container">
-
+	<div class="grid cols-2">
+		<div class="card hover dark">
+			<img class="full" src="./assets/images/parkings/iut.jpg" alt="car">
+			<div class="gradient alpha"></div>
+			<div class="tags top">
+				<span><img src="./assets/images/icons/place.svg" alt="place icon">160 places</span>
+			</div>
+			<div class="trip">
+				<span class="center">Parking 1</span>
+			</div>
+		</div>
+		<div class="card hover dark">
+			<img class="full" src="./assets/images/parkings/iut.jpg" alt="car">
+			<div class="gradient alpha"></div>
+			<div class="tags top">
+				<span><img src="./assets/images/icons/place.svg" alt="place icon">160 places</span>
+			</div>
+			<div class="trip">
+				<span class="center">Parking 1</span>
+			</div>
+		</div>
+		<div class="card hover dark">
+			<img class="full" src="./assets/images/parkings/iut.jpg" alt="car">
+			<div class="gradient alpha"></div>
+			<div class="tags top">
+				<span><img src="./assets/images/icons/place.svg" alt="place icon">160 places</span>
+			</div>
+			<div class="trip">
+				<span class="center">Parking 1</span>
+			</div>
+		</div>
+		<div class="card hover dark">
+			<img class="full" src="./assets/images/parkings/iut.jpg" alt="car">
+			<div class="gradient alpha"></div>
+			<div class="tags top">
+				<span><img src="./assets/images/icons/place.svg" alt="place icon">160 places</span>
+			</div>
+			<div class="trip">
+				<span class="center">Parking 1</span>
+			</div>
+		</div>
+	</div>
 </section>
