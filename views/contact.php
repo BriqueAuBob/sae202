0569616ad5a99b2feca9271bf596fbc63294e5d5
CassandreLAMATY <lamatycassandre@gmx.fr>
2023-06-09 12:20:59 +0200

    contact input fix

--- views/contact.php
+++ views/contact.php
@@ -1,16 +1,20 @@
 <header>
     <h1>Contactez-nous</h1>
+    <?= isset($_SESSION['contactLog']['confirmation']) ? '<p>' . $_SESSION['contactLog']['confirmation'] . '</p>' : '' ?>
     <?= isset($_SESSION['contactLog']['envoi']) ? '<p>' . $_SESSION['contactLog']['envoi'] . '</p>' : '' ?>
+    <?= isset($_SESSION['contactLog']['message']) ? '<p>' . $_SESSION['contactLog']['message'] . '</p>' : '' ?>
+    <?= isset($_SESSION['contactLog']['email']) ? '<p>' . $_SESSION['contactLog']['email'] . '</p>' : '' ?>
+    <?= isset($_SESSION['contactLog']['name']) ? '<p>' . $_SESSION['contactLog']['name'] . '</p>' : '' ?>
 </header>
 <section class="overlap container">
     <form class="form" method="post" action="envoi_mail.php">
         <div>
             <label for="name">Nom</label>
-            <input type="text" id="name" name="name" placeholder="John Doe" <?= isset($_SESSION['user']['name']) ? 'value="' . $_SESSION['user']['firstname'] . ' ' . $_SESSION['user']['name'] . '" disabled' : '' ?> required>
+            <input type="text" id="name" name="name" placeholder="John Doe" <?= isset($_SESSION['user']['name']) ? 'value="' . $_SESSION['user']['firstname'] . ' ' . $_SESSION['user']['name'] . '" readonly' : '' ?> required>
         </div>
         <div>
             <label for="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="john.doe@gmail.com" <?= isset($_SESSION['user']['name']) ? 'value="' . $_SESSION['user']['email'] . '" disabled' : '' ?> required>
+            <input type="email" id="email" name="email" placeholder="john.doe@gmail.com" <?= isset($_SESSION['user']['name']) ? 'value="' . $_SESSION['user']['email'] . '" readonly' : '' ?> required>
         </div>
         <div>
             <label for="message">Message</label>
@@ -19,3 +23,6 @@
         <button class="btn green" type="submit">Envoyer</button>
     </form>
 </section>
+<?php
+    unset($_SESSION['contactLog']);
+?>
