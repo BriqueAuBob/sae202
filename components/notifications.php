db6b2f0b00ec71a8eb4289031fa60ea4e94f086d
BriqueAuBob <briqueaubob@gmail.com>
2023-06-12 15:08:05 +0200

    notifs

---
+++ components/notifications.php
@@ -0,0 +1,35 @@
+<?php
+enum NotificationType: string
+{
+    case SUCCESS = 'success';
+    case ERROR = 'error';
+    case INFO = 'info';
+}
+
+function displayNotification(NotificationType $type, $message)
+{
+    $icon = match ($type) {
+        NotificationType::SUCCESS => 'check',
+        NotificationType::ERROR => 'cross',
+        NotificationType::INFO => 'info',
+    };
+    echo '<li class="notification ' . $type->value . '">
+    <a href="#">
+        <img src="./assets/images/icons/' . $icon . '.svg" alt="Check icon">
+        <p>' . $message . '</p>
+    </a>
+</li>';
+}
+?>
+<div class="popover-wrapper">
+    <button class="no-style"><img src="./assets/images/icons/bell.svg" alt="Bell icon"></button>
+    <div class="popover">
+        <ul class="notifications">
+            <?php
+            displayNotification(NotificationType::SUCCESS, 'Vous avez une nouvelle réservation pour votre trajet de la part de Melvil.');
+            displayNotification(NotificationType::ERROR, 'Jade a annulé sa réservation.');
+            displayNotification(NotificationType::INFO, 'Cassandre vous a envoyé un message.');
+            ?>
+        </ul>
+    </div>
+</div>
